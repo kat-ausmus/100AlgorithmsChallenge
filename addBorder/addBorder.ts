d4e63bc60a9d8fd316475c5e52ab6ff1d8737cb5
Kat Ausmus <katausmus@gmail.com>
2019-09-17 11:21:49 -0500

    2nd push

--- addBorder/addBorder.ts
+++ addBorder/addBorder.ts
@@ -1,5 +1,27 @@
-function addBorder(picture: string[]): string[] {
 
-}
+const longestStrLength = strArray => {
+    return strArray.reduce( (accumulator, str)=> {
+        return  accumulator < str.length ? str.length : accumulator;
+    }, 0)
+};
 
-// console.log(addBorder(["abc", "ded"]));
+const createBorder = (character, length) => {
+    return character.repeat(length);
+};
+
+
+const addBorder = (picture: string[]): string[] => {
+    const bordered = [];
+    const wordLen = longestStrLength(picture);
+    const asterisk = "*";
+    const border = createBorder(asterisk, wordLen+2);
+    bordered.push(border);
+    picture.forEach( word => {
+        bordered.push(`${asterisk}${word}${asterisk}`);
+    });
+    bordered.push(border);
+
+    return bordered;
+};
+
+console.log(addBorder(["abc", "ded"]));
