1cd82a08ac3afde4ac32094bcbf1a8025f6d4ec0
Kat Ausmus <katausmus@gmail.com>
2019-09-17 10:19:02 -0500

    first push to 100AlgoChallenge

--- absoluteValuesSumMinization/absoluteValuesSumMinization.ts
+++ absoluteValuesSumMinization/absoluteValuesSumMinization.ts
@@ -1,8 +1,10 @@
 function absoluteValuesSumMinimization(a: number[]): number {
-
+    const isEven = a.length % 2 === 0;
+    const arrLen = a.length;
+    return isEven? a[arrLen / 2 -1]: a[Math.floor(arrLen/2)]
 }
 
-// console.log(absoluteValuesSumMinimization([2, 4, 7]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
+console.log(absoluteValuesSumMinimization([2, 4, 7]));
+console.log(absoluteValuesSumMinimization([2, 4, 6, 7]));
+console.log(absoluteValuesSumMinimization([2, 4, 6, 6, 7]));
+console.log(absoluteValuesSumMinimization([2, 4,  6, 6, 7, 8]));
