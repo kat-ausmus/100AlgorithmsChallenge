56521051a0b659b4dfc326d8110adb87bb102744
Kat Ausmus <katausmus@gmail.com>
2019-09-24 17:38:19 -0500

    update 2 algorithms

--- arrayMaxConsecutiveSum/arrayMaxConsecutiveSum.js
+++ arrayMaxConsecutiveSum/arrayMaxConsecutiveSum.js
@@ -1,5 +1,42 @@
-function arrayMaxConsecutiveSum(inputArray, k) {
+const getAdjacentSum = ({ start, numElements, inputArray }) => {
+    const len = inputArray.length;
+    const lastPosition = start + numElements < len ? start + numElements : len;
+    // console.log(`lastPosition  is ${lastPosition}`);
+    let sum = undefined;
+    for (let begin = start; begin < lastPosition; begin++) {
+        const element = inputArray[begin];
+        if (!sum) {
+            sum = element;
+        } else {
+          sum += element;
+        }
+    }
+    return sum;
+};
 
-}
+const arrayMaxConsecutiveSum = (inputArray, k) => {
+    if (!inputArray) {
+        throw new Error('input Array is empty or not defined');
+    }
+    // console.log(`******************`);
+    const len = inputArray.length;
+    let highestSum = undefined;
+
+    for (let i = 0; i < len; i++) {
+        const adjacentSum = getAdjacentSum({ start: i, numElements: k, inputArray });
+        // console.log(`adjacent sum is ${adjacentSum}`);
+        if (!highestSum) {
+            highestSum = adjacentSum;
+        } else {
+            highestSum = highestSum > adjacentSum ? highestSum : adjacentSum;
+        }
+        // console.log(`highest sum is ${highestSum}`);
+    }
+    return highestSum;
+};
 
 console.log(arrayMaxConsecutiveSum([2, 3, 5, 1, 6], 2));
+
+console.log(arrayMaxConsecutiveSum([2, 3, 5, 1, 6], 3));
+
+console.log(arrayMaxConsecutiveSum([2, 3, 5, 1, 6], 4));
--- arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.js
+++ arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.js
@@ -1,5 +1,28 @@
-function arrayMaximalAdjacentDifference(inputArray) {
+const arrayMaximalAdjacentDifference = inputArray => {
+    if (!inputArray) {
+        throw new Error('input Array is empty or not defined');
+    }
+    const len = inputArray.length;
+    let highestDifference = undefined;
 
-}
+    for (let i = 0; i < len; i++) {
+        const element = inputArray[i];
+        const j = i + 1;
+        if (j === len) {
+            continue;
+        }
+        const adjacentElement = inputArray[j];
+        const diff = Math.abs(element - adjacentElement);
+
+        if (!highestDifference) {
+            highestDifference = diff;
+        } else {
+            highestDifference = highestDifference > diff ? highestDifference : diff;
+        }
+    }
+    return highestDifference;
+};
 
 console.log(arrayMaximalAdjacentDifference([2, 4, 1, 0]));
+
+console.log(arrayMaximalAdjacentDifference([-15, 0, 2, 4, 1, 5, 15, 45]));
