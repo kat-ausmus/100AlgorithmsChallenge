ac85966aaeffdeb18f03c62a61e12f014a1fb658
kat-ausmus <katausmus@gmail.com>
2019-09-29 21:28:15 -0500

    array Previous

--- arrayReplace/arrayReplace.js
+++ arrayReplace/arrayReplace.js
@@ -1,5 +1,14 @@
-function arrayReplace(inputArray, elemToReplace, substitutionElem) {
+const  arrayReplace = (inputArray, elemToReplace, substitutionElem) => {
+    return inputArray.reduce( (accumulator, element) => {
+        if( element === elemToReplace){
+            accumulator.push(substitutionElem)
+        } else {
+          accumulator.push(element);
+        }
+        return accumulator;
+    },[])
 
-}
+};
 
 console.log(arrayReplace([1, 2, 1], 1, 3));
+console.log(arrayReplace([1, 2, 1, 5, 11, 34, 56,2,3], 2, 100));
--- avoidObstacles/avoidObstacles.js
+++ avoidObstacles/avoidObstacles.js
@@ -1,5 +1,13 @@
-function avoidObstacles(inputArray) {
+const avoidObstacles = inputArray => {
+    inputArray = inputArray.sort((a, b) => a - b);
+    const len = inputArray.length;
+    const largestVal = inputArray[len - 1];
 
-}
+    for (let i = 1; i < largestVal+1; i++) {
+        if (inputArray.every(element => element % i !== 0)) {
+            return i;
+        }
+    }
+};
 
 console.log(avoidObstacles([5, 3, 6, 7, 9]));
