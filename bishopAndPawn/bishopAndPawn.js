1ee0c649c13fce235c0c48a241f21227fcd46224
kat-ausmus <katausmus@gmail.com>
2019-10-01 16:55:22 -0500

    Bs

--- bishopAndPawn/bishopAndPawn.js
+++ bishopAndPawn/bishopAndPawn.js
@@ -1,5 +1,27 @@
-function bishopAndPawn(bishop, pawn) {
+const board = {
+    a: 1,
+    b: 2,
+    c: 3,
+    d: 4,
+    e: 5,
+    f: 6,
+    g: 7,
+    h: 8
+};
 
-}
+const bishopAndPawn = (bishop, pawn) => {
+    const bishopX = board[bishop[0]];
+    const bishopY = parseInt(bishop[1],10);
+
+    const pawnX = board[pawn[0]];
+    const pawnY = parseInt(pawn[1],10);
+
+    const leftSide = bishopX + bishopY === pawnY + pawnX;
+    const rightSide = bishopX + pawnY === pawnX + bishopY;
+    return leftSide || rightSide;
+};
 
 console.log(bishopAndPawn('a1', 'c3'));
+console.log(bishopAndPawn('d4', 'a1'));
+console.log(bishopAndPawn('d4', 'g1'));
+console.log(bishopAndPawn('d4', 'g2'));
--- boxBlur/boxBlur.js
+++ boxBlur/boxBlur.js
@@ -1,7 +1,32 @@
-function boxBlur(image) {
+const boxBlur = image => {
+    const resolution = [];
+    // look at y coords
+    // length -2 to avoid the edges
+    console.log(`image has length ${image.length}`);
+    for (let y = 0; y < image.length - 2; y++) {
+        // console.log(`---------`);
+        // console.log(`y is ${y}`);
+        const line = [];
+        for (let x = 0; x < image[y].length - 2; x++) {
+            // console.log(`x is ${x}`);
+            let sum = 0;
+            let count = 0;
+            // 3x3 pixel
+            for (let a = y; a < y + 3; a++) {
+                // console.log(`a is ${a}`);
+                for (let b = x; b < x + 3; b++) {
+                    // console.log(`b is ${b}`);
+                    sum += image[a][b];
+                    count++;
+                }
+            }
+            line.push(Math.floor(sum / count));
+        }
+        resolution.push(line);
+    }
+    return resolution;
+};
 
-}
+console.log(boxBlur([[1, 1, 1], [1, 7, 1], [1, 1, 1]]));
 
-console.log(boxBlur([[1, 1, 1],
-    [1, 7, 1],
-    [1, 1, 1]]));
+console.log(boxBlur([[1, 1, 1,4], [1, 7, 1,4], [1, 1, 1,4]]));
